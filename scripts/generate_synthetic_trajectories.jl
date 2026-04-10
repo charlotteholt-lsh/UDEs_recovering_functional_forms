552983235b1c255d4bb48e9cb40ef4bca9ca22fb
Charlotte Holt <charlotte.holt@lshtm.ac.uk>
2026-04-10 15:48:25 +0100

    Add completed code for synthesising trajectories

--- scripts/generate_synthetic_trajectories.jl
+++ scripts/generate_synthetic_trajectories.jl
@@ -15,45 +15,53 @@ using DrWatson
 using CSV
 using JLD2
 using DataFrames
-
-# Define parameters
-
-# Massachusetts population size - taken from JHU CSSE
-N = 6892503
-
-# Initial conditions
-E0 = 0.0
-R0 = 0.0
-D0 = 0.0
-
-
-# Latent period of 3 days represented by incubation rate sigma
-const sigma = 1/3 
-# Infectious period of 10 days represented by recovery rate gamma
-const gamma = 1/10
+using DifferentialEquations
+using ComponentArrays
+using DSP
+using Plots 
 
 #========================================================
 HELPER FUNCTIONS
 =========================================================#
 
+# Calculate a 7-day moving average
 function moving_average(x)
     k = 7
     kernel = ones(k) / k
     if length(x) < k
         return zeros(length(x))
     end
-    valid = conv(x, kernel)
+    valid = DSP.conv(x, kernel)[k:end-k+1]
+    # Returns a vector of length(x) where the first k-1 entries are zero
     return vcat(zeros(k-1), valid)
 end
 
-function extract_param_values(beta_form, location)
-    # Only undertake for MA
-    dt_location = "MA"
-    model_name = "exp"
-    RESULT = 1
-    posterior_path = f"../results/{gt_location}/{model_name}_posterior_{RESULT}.csv"
+# Build the file name for simulation
+function sim_name(fixed_p, estimated_p)
+    params = (
+        pop  = Int(round(fixed_p.population)),
+        E0   = round(fixed_p.E0, sigdigits=5),
+        R0   = round(fixed_p.R0_recovered, sigdigits=5),
+        D0   = round(fixed_p.D0, sigdigits=5),
+        sig  = round(fixed_p.sigma, sigdigits=5),
+        gam  = round(fixed_p.gamma, sigdigits=5),
+        zet  = round(fixed_p.zeta, sigdigits=5),
+        prev = round(estimated_p.prevalence, sigdigits=5),
+        del  = round(estimated_p.delta, sigdigits=5),
+        R0r  = round(estimated_p.R0_reproduction, sigdigits=5),
+    )
+    return "synthetic_" * savename(params; connector="_", sort=false)
 end
 
+function convert_to_daily_and_smooth(traj, smoothing_window)
+    # Convert to daily values by taking the difference between consecutive entries
+    daily_values = [0.0; diff(traj)]
+    # Smooth the daily values using a moving average
+    smoothed_daily_values = moving_average(daily_values)
+    # Remove the first few entries that are zero due to the moving average
+    smoothed_daily_values = smoothed_daily_values[smoothing_window:end]
+    return smoothed_daily_values
+end
 
 #========================================================
 DEFINE THE MODEL
@@ -93,7 +101,7 @@ FUNCTION TO RUN THE MODEL
 # p must be of the form (beta0, delta, sigma, gamma, zeta)
 # fixed_p must be of the form (sigma, gamma, zeta, population, E0, R0_recovered, D0)
 # estimated_p must be of the form (prevalence, delta, R0_reproduction)
-function run_seird_functional_form(fixed_p, estimated_p, train_length, smoothing_window = 7)
+function run_seird_functional_form(fixed_p, estimated_p, obs_length, smoothing_window = 7)
 
     # Retrieve fixed parameters
     sigma = fixed_p.sigma
@@ -117,11 +125,18 @@ function run_seird_functional_form(fixed_p, estimated_p, train_length, smoothing
     # Define initial state
     init_state = [S0, E0, I0, R0_recovered, D0]
     # Define parameters for the ODE solver
-    p = (beta0, delta, sigma, gamma, zeta)
+    p = ComponentArray(
+        beta0 = beta0,
+        delta = delta,
+        sigma = sigma,
+        gamma = gamma,
+        zeta = zeta
+    )
 
     # Simulate extra days for the first 6 days to avoid errors when calculating the moving average
     extra_days = smoothing_window - 1
-    tspan = (0, train_length + extra_days)
+    # Remove a day so when we take the difference we have the obs_length number of data points
+    tspan = [0, obs_length + extra_days -1]
 
     # Define ODE problem 
     prob = ODEProblem(seird_functional!, init_state, tspan, p)
@@ -130,6 +145,74 @@ function run_seird_functional_form(fixed_p, estimated_p, train_length, smoothing
     
     return sol
 end
+
+function generate_synthetic_data(fixed_p, estimated_p, obs_length, smoothing_window = 7)
+    # Run model
+    sim = run_seird_functional_form(fixed_p, estimated_p, obs_length, smoothing_window)
+
+    # Extract raw states 
+    s_traj = sim[1, :]
+    e_traj = sim[2, :]
+    i_traj = sim[3, :]
+    r_traj = sim[4, :]
+    d_traj = sim[5, :]
+
+
+
+    # Save the result
+	fname = sim_name(fixed_p, estimated_p) * ".jld2"
+
+	mkpath(datadir("synthesised_trajectories"))
+
+
+	save(datadir("synthesised_trajectories", fname),
+		"fixed_p", fixed_p, "estimated_p", estimated_p, "days", 1:obs_length, 
+        "susceptible", s_traj, "exposed", e_traj, "infectious", i_traj, "recovered", r_traj, "deaths", d_traj)
+
+    println("Finished generating synthetic data for $(fname)")
+
+    return s_traj, e_traj, i_traj, r_traj, d_traj
+
+end
+
+
+#========================================================
+DEFINE PARAMETERS AND GENERATE SYNTHETIC DATA
+=========================================================#
+
+# Define parameters
+
+# Massachusetts population size - taken from JHU CSSE
+N = 6892503
+
+# Initial conditions
+E0 = 0.0
+R0 = 0.0
+D0 = 0.0
+
+# Latent period of 3 days represented by incubation rate sigma
+const sigma = 1/3 
+# Infectious period of 10 days represented by recovery rate gamma
+const gamma = 1/10
+
+
+# EXTRACTED USING GROUND_TRUTH_VALUES FOR MA IN THE PYTHON CODE
+prevalence = exp(-11.468967)
+R0_reproduction = 5.284404
+const delta = exp(-8.941224)
+
+fixed_p = ComponentArray(sigma = sigma, 
+                        gamma = gamma, 
+                        zeta = 0.02, 
+                        population = N,
+                        E0 = E0, 
+                        R0_recovered = R0, 
+                        D0 = D0)
+estimated_p = ComponentArray(prevalence = prevalence, 
+                            delta = delta, 
+                            R0_reproduction = R0_reproduction)
+
+generate_synthetic_data(fixed_p, estimated_p, 365)
 
 
 # Convert csv to jld2 file
@@ -140,3 +223,12 @@ df = DataFrame(datafile)
 
 # Save the dataframe as a jld2 file
 save(datadir("sims", "synthetic_mortality_ground_truth_exp.jld2"), "df", df)
+
+
+# Extract infectious trajectoryfrom JLD2 file
+dataset = load(datadir("synthesised_trajectories", "synthetic_pop=6892503_E0=0.0_R0=0.0_D0=0.0_sig=0.333_gam=0.1_zet=0.02_prev=1.04e-5_del=0.000131_R0r=5.28.jld2"))
+
+
+# Plot infections
+infections = dataset["infectious"]
+display(plot(1:length(infections), infections, label="Generated data", xlabel="Days", ylabel="Infections", title="Infections over time"))
